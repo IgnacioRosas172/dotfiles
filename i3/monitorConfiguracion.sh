e561b0aebad044dda7b7b8c679bdfdacea58bcda
IgnacioRosas172 <ignacio105garcia@gmail.com>
2023-08-04 13:37:05 -0600

    configuracion de i3 y de los monitores

---
+++ i3/monitorConfiguracion.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 -r 143.99 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1080 -r 144.00yy --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
